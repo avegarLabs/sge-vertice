494e56e0072988d71496a5f3fdd69ae89957c521
Gustavo Ernesto Martinez Mir <gustavo.martinez@vertice.cu>
2021-04-01 10:29:27 -0400

    Modificando capacitacion

--- prenomina15/views.py
+++ prenomina15/views.py
@@ -2872,6 +2872,7 @@ def request_report_pren86(fecha_inic, fecha_fin, obra, request):
             no_loop += 1
             tarifa_se = (element.salario_escala_ref / Decimal(190.60)).quantize(Decimal('.000000001'),
                                                                             rounding=ROUND_HALF_UP)
+            trab_cat_cien = 0.00
             if element.sal_cat_cient != 0.00:
                 trab_cat_cien = Trabajador.objects.filter(id=element.trabajador_id).get().salario_cat_cient
                 tarifa_maest = (element.trab_cat_cien / Decimal(190.60)).quantize(Decimal('.000000001'),
