d86ba0cb1075e449b44811d2c7f218edcab1d0e8
Gustavo Ernesto Martinez Mir <gustavo.martinez@vertice.cu>
2021-07-21 10:41:30 -0400

    Modificando entrada de datos a contratacion

--- entrada_datos/models.py
+++ entrada_datos/models.py
@@ -38,22 +38,14 @@ class Servicio (BaseUrls, models.Model):
 
 class OT(BaseUrls, models.Model):
     codigo_ot = models.CharField(max_length=10, unique=True)
-    descripcion_ot = models.CharField(verbose_name='Descripción del Servicio', max_length=100, null=False, blank=False, default='')
-    alcance = models.CharField(verbose_name='Alcance', max_length=200, null=False, blank=False, default='')
+    descripcion_ot = models.CharField(max_length=100, null=False, blank=False)
     no_contrato = models.CharField(max_length=5, null=False, blank=False, unique=True)
-    valor_contrato = models.DecimalField(max_digits=9, decimal_places=2, null=False, default=0.00)
+    valor_contrato = models.DecimalField(max_digits=9, decimal_places=2, editable=False, default=0.00)
     tipo_servicio = models. ForeignKey(Servicio, on_delete=models.DO_NOTHING, default='')
     area = models.ForeignKey(Area, on_delete=models.DO_NOTHING, default='')
     inversionista = models.ForeignKey(Inversionista, on_delete=models.DO_NOTHING, default='')
     OPT_Unidad = (('03', 'USTI'), ('07', 'UGDD'))
     unidad = models.CharField(max_length=4, choices=OPT_Unidad, default='', null=False, blank=False)
-    fecha_recepcion = models.DateField(verbose_name='Fecha de Recepción de Solicitud', null=True, blank=True)
-    fecha_aprobacion = models.DateField(verbose_name='Fecha de Aprobación de Solicitud', null=True, blank=True)
-    fecha_entrega_pt = models.DateField(verbose_name='Fecha de Entrega de la PT', null=True, blank=True)
-    fecha_terminado_contrato = models.DateField(verbose_name='Fecha de Terminado Contrato', null=True, blank=True)
-    fecha_entrega_cliente = models.DateField(verbose_name='Fecha Entrega al Cliente', null=True, blank=True)
-    fecha_firma_contrato = models.DateField(verbose_name='Fecha de Firma del Contrato', null=True, blank=True)
-    fecha_recepcion_contrato = models.DateField(verbose_name='Fecha de Recepción del Contrato', null=True, blank=True)
 
 
     def __str__(self):
