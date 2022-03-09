f3fb642ca61f672a939ec6f0fb723b66de8f52bd
Rolando Michel Fernández Pérez <rolando.fernandez@vertice.cu>
2022-03-09 07:19:55 -0500

    Correcciones agregar trabajador

--- rechum/settings.py
+++ rechum/settings.py
@@ -106,12 +106,17 @@ WSGI_APPLICATION = 'rechum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rechum',
+        # 'NAME': 'rechum',
         # 'NAME': 'sge_gemm',
-        'HOST': '192.168.1.155',
-        'USER': 'sge.vertice',
-        'PASSWORD': 'Ve2020*',
-        'PORT': '2345'
+        # 'HOST': '192.168.1.155',
+        # 'USER': 'sge.vertice',
+        # 'PASSWORD': 'Ve2020*',
+        # 'PORT': '2345'
+        'NAME': 'rechum_r',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432'
     }
 }
 
